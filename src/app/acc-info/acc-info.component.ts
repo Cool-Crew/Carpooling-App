29f5c42d827d7053b914e5ccd4aca4dd9f0e0117
roshaunr <73445538+roshaunr@users.noreply.github.com>
2023-05-29 14:05:02 -0400

    Update acc-info.component.ts
    Fixed Ruben's acc-info route

--- src/app/acc-info/acc-info.component.ts
+++ src/app/acc-info/acc-info.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import { AuthService } from '../auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../auth.service";
 
 @Component({
-  selector: 'app-acc-info',
-  templateUrl: './acc-info.component.html',
-  styleUrls: ['./acc-info.component.css']
+  selector: "app-acc-info",
+  templateUrl: "./acc-info.component.html",
+  styleUrls: ["./acc-info.component.css"],
 })
 export class AccInfoComponent implements OnInit {
   user: any;
@@ -13,17 +13,6 @@ export class AccInfoComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
-    if (this.authService.isAuthenticated()) {
-      this.authService.getAccountInfo().subscribe(
-        data => {
-          this.user = data;
-        },
-        err => {
-          console.log(err);
-        }
-      );
-    } else {
-      this.router.navigate(['/login']);
-    }
+    this.user = this.authService.readToken();
   }
 }
