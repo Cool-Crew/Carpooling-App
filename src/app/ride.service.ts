74253b6064bf09cf2627cea0e5233014bbc9a06d
roshaunr <73445538+roshaunr@users.noreply.github.com>
2023-07-09 14:13:45 -0400

    Now adds Location information when adding a rider to a ride
    Place holder is being used in the ride-card.component for location as of this point

--- src/app/ride.service.ts
+++ src/app/ride.service.ts
@@ -5,55 +5,81 @@ import { environment } from "../environments/environment";
 import { AuthService } from "./auth.service";
 import { Ride } from "./Ride";
 
-
 @Injectable({ providedIn: "root" })
 export class RideService {
   constructor(private http: HttpClient, private auth: AuthService) {}
 
-  async getRides(): Promise<{message: String, _rides:[Ride]} | undefined> {
+  async getRides(): Promise<{ message: String; _rides: [Ride] } | undefined> {
     const token = this.auth.getToken();
     if (token) {
       const headers = { Authorization: `JWT ${token}` };
-      return this.http.get<{message:String, _rides:[Ride]}>(`${environment.userAPIBase}/rides`, { headers }).toPromise();
+      return this.http
+        .get<{ message: String; _rides: [Ride] }>(
+          `${environment.userAPIBase}/rides`,
+          { headers }
+        )
+        .toPromise();
     }
-    return ;
+    return;
   }
 
-  rmRiderFromRide(rideId: any, riderId: any):Observable<any>{
+  rmRiderFromRide(rideId: any, riderId: any): Observable<any> {
     const token = this.auth.getToken();
 
-    if (token){
-      const headers = {Authorization: `JWT ${token}`};
-      return this.http.delete<{message:String}>(`${environment.userAPIBase}/rides/${rideId}/riders/${riderId}`, { headers });
+    if (token) {
+      const headers = { Authorization: `JWT ${token}` };
+      return this.http.delete<{ message: String }>(
+        `${environment.userAPIBase}/rides/${rideId}/riders/${riderId}`,
+        { headers }
+      );
     }
 
     return new Observable();
   }
 
-  registerDriverToRide(rideId: any, driverData: any):Observable<any> {
+  registerDriverToRide(rideId: any, driverData: any): Observable<any> {
     const token = this.auth.getToken();
     if (token) {
-      const headers = {Authorization: `JWT ${token}`};
-      return this.http.post<{message:String}>(`${environment.userAPIBase}/rides/${rideId}/driver`, {ride:rideId, newDriver: driverData}, { headers });
+      const headers = { Authorization: `JWT ${token}` };
+      return this.http.post<{ message: String }>(
+        `${environment.userAPIBase}/rides/${rideId}/driver`,
+        { ride: rideId, newDriver: driverData },
+        { headers }
+      );
     }
     return new Observable();
   }
 
-  rmDriverFromRide(rideId: any):Observable<any> {
-    const token = this.auth.getToken();
-    if (token){
-      const headers = {Authorization: `JWT ${token}`};
-      return this.http.delete<{message:String}>(`${environment.userAPIBase}/rides/${rideId}/driver`, { headers });
-    }
-
-    return new Observable();
-  }
-
-  registerRidertoRide(rideId: any, riderId: any):Observable<any>{
+  rmDriverFromRide(rideId: any): Observable<any> {
     const token = this.auth.getToken();
     if (token) {
-      const headers = {Authorization: `JWT ${token}`};
-      return this.http.post<{message:String}>(`${environment.userAPIBase}/rides/:${rideId}/riders`, {ride:rideId, newRider: riderId}, { headers });
+      const headers = { Authorization: `JWT ${token}` };
+      return this.http.delete<{ message: String }>(
+        `${environment.userAPIBase}/rides/${rideId}/driver`,
+        { headers }
+      );
+    }
+
+    return new Observable();
+  }
+
+  registerRidertoRide(
+    rideId: any,
+    riderId: any,
+    pickupLocation: any
+  ): Observable<any> {
+    const token = this.auth.getToken();
+    if (token) {
+      const headers = { Authorization: `JWT ${token}` };
+      const riderData = {
+        riderID: riderId,
+        pickupLocation: pickupLocation,
+      };
+      return this.http.post<{ message: string }>(
+        `${environment.userAPIBase}/rides/${rideId}/riders`,
+        { ride: rideId, newRider: riderData },
+        { headers }
+      );
     }
     return new Observable();
   }
