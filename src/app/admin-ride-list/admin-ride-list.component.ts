df45c0a13b5559b0ae65c812866f6d51fceb1bea
Olivia Brown <oliviataylor437@gmail.com>
2023-07-30 19:01:09 -0400

    date filtering added

--- src/app/admin-ride-list/admin-ride-list.component.ts
+++ src/app/admin-ride-list/admin-ride-list.component.ts
@@ -28,27 +28,30 @@ export class AdminRideListComponent {
 
   constructor(private rideService: RideService) { }
 
-  async ngOnInit(): Promise<void> {
+  async getRides(): Promise<void> {
     let res: {message: String, _rides: [Ride]} | undefined = await this.rideService.getRides(); 
     this.rides = res?._rides;
     // replaced ids with usernames
     if (this.rides) {
-      // Sort rides by date
-      this.rides.sort((a, b) => {
-        const aDateTime = a.dateTime ?? ""; // Provide a default value if a.dateTime is undefined
-        const bDateTime = b.dateTime ?? ""; // Provide a default value if b.dateTime is undefined
+        // Sort rides by date
+        this.rides.sort((a, b) => {
+          const aDateTime = a.dateTime ?? ""; // Provide a default value if a.dateTime is undefined
+          const bDateTime = b.dateTime ?? ""; // Provide a default value if b.dateTime is undefined
 
-        if (aDateTime < bDateTime) {
-          return -1;
-        } else if (aDateTime > bDateTime) {
-          return 1;
-        } else {
-          return 0;
-        }
+          if (aDateTime < bDateTime) {
+            return -1;
+          } else if (aDateTime > bDateTime) {
+            return 1;
+          } else {
+            return 0;
+          }
       });
 
-
       for (const ride of this.rides){
+        //convert dateTime to actual date object
+        if (ride.dateTime){
+          ride.dateTime = new Date(ride.dateTime);
+        }
 
         // get username for creator
         let res: {message: string, _usernames: Usernames} | undefined = await this.rideService.getUsernames(ride.creator);
@@ -73,13 +76,75 @@ export class AdminRideListComponent {
             }
           }
         }
-        
       }
     }
+  }  
+
+  async ngOnInit(): Promise<void> {
+    await this.getRides();
   }
 
-  updateRange(): void {
+  async updateRange(): Promise<void> {
 
+    await this.getRides();
+    console.log(this.range);
+
+    //if range = -1, show all future rides
+    if (this.range == -1){
+      let now = new Date();
+      this.rides = this.rides?.filter(ride => {
+        
+        return ride.dateTime && ride.dateTime > now
+        
+      });
+
+      console.log(this.rides);
+    }
+
+    //if range = 333, show all rides
+    if (this.range == 333){
+      //no filter
+    }
+
+    //if range = 1, show rides today
+    if (this.range == 0){
+      let now = new Date();
+      let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1);
+      this.rides = this.rides?.filter(ride => ride.dateTime && (ride.dateTime >= today) && (ride.dateTime < tomorrow));
+    }
+
+    //if range = 7, show rides from the last 7 days
+    if (this.range == 7){
+      let now = new Date();
+      let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      let lastWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate()-7);
+      this.rides = this.rides?.filter(ride => ride.dateTime && (ride.dateTime >= lastWeek) && (ride.dateTime < today));
+    }
+
+    //if range = 30, show rides from the last 30 days
+    if (this.range == 30){
+      let now = new Date();
+      let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      let lastMonth = new Date(now.getFullYear(), now.getMonth()-1, now.getDate());
+      this.rides = this.rides?.filter(ride => ride.dateTime && (ride.dateTime >= lastMonth) && (ride.dateTime < today));
+    }
+
+    //if range = 180, show rides from the last 180 days
+    if (this.range == 180){
+      let now = new Date();
+      let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      let lastMonth = new Date(now.getFullYear(), now.getMonth()-6, now.getDate());
+      this.rides = this.rides?.filter(ride => ride.dateTime && (ride.dateTime >= lastMonth) && (ride.dateTime < today));
+    }
+
+    //if range = 365, show rides from the last 365 days
+    if (this.range == 365){
+      let now = new Date();
+      let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      let lastMonth = new Date(now.getFullYear()-1, now.getMonth(), now.getDate());
+      this.rides = this.rides?.filter(ride => ride.dateTime && (ride.dateTime >= lastMonth) && (ride.dateTime < today));
+    }
   }
   
   cancelRide(rideId: string): void {
